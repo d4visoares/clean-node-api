f3142a6875ad96e474e898d8445b844f152c1f8e
Davi-Soares-Silva <davisoares4456@gmail.com>
2023-06-27 20:15:01 -0300

    refactor: move duplicated code to a utils method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,18 +1,12 @@
 import { MissingParamError } from '../errors';
 import { HttpRequest, HttpResponse } from '../protocols';
+import { badRequest } from '../utils';
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name'),
-      };
-    }
+    if (!httpRequest.body.name)
+      return badRequest(new MissingParamError('name'));
 
-    return {
-      statusCode: 400,
-      body: new MissingParamError('email'),
-    };
+    return badRequest(new MissingParamError('email'));
   }
 }
