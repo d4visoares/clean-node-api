49e821680e5301d85c72388d1e8b4b5b680770c4
Davi-Soares-Silva <davisoares4456@gmail.com>
2023-07-11 21:12:34 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,16 @@
+import { DbAddAccount } from '@/data/usecases';
+import { BcryptAdapter } from '@/infra/cryptography';
+import { AccountMongoRepository } from '@/infra/db/mongodb';
+import { SignUpController } from '@/presentation/controllers';
+import { EmailValidatorAdapter } from '@/utils/adapters';
+
+export const makeSignUpController = () => {
+  const SALT = 12;
+
+  const dbAddAccount = new DbAddAccount(
+    new BcryptAdapter(SALT),
+    new AccountMongoRepository()
+  );
+
+  return new SignUpController(new EmailValidatorAdapter(), dbAddAccount);
+};
