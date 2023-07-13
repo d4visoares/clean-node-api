410605b3138bd106dcc43d137e3e018df90e95cb
Davi-Soares-Silva <davisoares4456@gmail.com>
2023-07-12 21:27:26 -0300

    feat: ensure MongoHelper reconnect if client is down

--- src/infra/db/mongodb/utils/mongo-helper.ts
+++ src/infra/db/mongodb/utils/mongo-helper.ts
@@ -1,11 +1,15 @@
 import { Collection, MongoClient } from 'mongodb';
 
+import { MONGO } from '../../../../utils/constants';
+
 export class MongoHelper {
   private static instance: MongoHelper;
-  private client: MongoClient | null;
+  private uri: string | null;
+  client: MongoClient | null;
 
   constructor() {
     this.client = null;
+    this.uri = null;
   }
 
   static getInstance(): MongoHelper {
@@ -16,21 +20,31 @@ export class MongoHelper {
     return this.instance;
   }
 
-  async connect(uri: string) {
-    this.client = await MongoClient.connect(uri);
-  }
-
-  async disconnect() {
-    if (!this.client) throw new Error('MISSING_CLIENT');
-
-    await this.client.close();
+  clearConnection() {
+    this.uri = null;
     this.client = null;
   }
 
-  async getCollection(name: string): Promise<Collection | undefined> {
-    if (!this.client) throw new Error('MISSING_CLIENT');
+  async connect(uri: string | null) {
+    this.uri = uri;
+    this.client = await MongoClient.connect(uri || MONGO.URL);
+  }
 
-    return this.client.db().collection(name);
+  async disconnect() {
+    if (this.client) {
+      await this.client.close();
+      this.clearConnection();
+    }
+  }
+
+  async getCollection(name: string): Promise<Collection | undefined> {
+    if (!this.client) {
+      await this.connect(this.uri);
+    }
+
+    if (this.client) {
+      return this.client.db().collection(name);
+    }
   }
 
   map(collection: any): any {
