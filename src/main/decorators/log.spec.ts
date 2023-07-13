1a98570851356685240f7c2a442e7f35adbf3d39
Davi-Soares-Silva <davisoares4456@gmail.com>
2023-07-13 20:03:09 -0300

    test: ensure LogControllerDecorator calls controller handle with correct values

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -2,24 +2,36 @@ import { Controller, HttpRequest, HttpResponse } from '@/presentation';
 
 import { LogControllerDecorator } from './log';
 
+const makeControllerStub = () => {
+  class ControllerStub implements Controller {
+    handle(_: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {},
+      };
+
+      return new Promise((resolve) => resolve(httpResponse));
+    }
+  }
+
+  return new ControllerStub();
+};
+
+const makeSut = () => {
+  const controllerStub = makeControllerStub();
+  const logControllerDecorator = new LogControllerDecorator(controllerStub);
+
+  return {
+    sut: logControllerDecorator,
+    controllerStub,
+  };
+};
+
 describe('LogControllerDecorator', () => {
   test('Should call controller handle with correct values', async () => {
-    class ControllerStub implements Controller {
-      handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {},
-        };
-
-        return new Promise((resolve) => resolve(httpResponse));
-      }
-    }
-
-    const controllerStub = new ControllerStub();
+    const { sut, controllerStub } = makeSut();
 
     const handleSpy = jest.spyOn(controllerStub, 'handle');
-
-    const sut = new LogControllerDecorator(controllerStub);
 
     const httpRequest = {
       body: {
