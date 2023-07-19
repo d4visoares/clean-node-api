b849fc0a3a9fafe7acc5b51d599104da992b7c66
Davi-Soares-Silva <davisoares4456@gmail.com>
2023-07-19 20:06:56 -0300

    test: ensure SingUpController returns 400 if validation fails

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -248,4 +248,18 @@ describe('SignUp Controller', () => {
 
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
   });
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut();
+
+    jest
+      .spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new MissingParamError('any_field'));
+
+    const httpResponse = await sut.handle(makefakeHttpRequest());
+
+    expect(httpResponse).toEqual(
+      badRequest(new MissingParamError('any_field'))
+    );
+  });
 });
