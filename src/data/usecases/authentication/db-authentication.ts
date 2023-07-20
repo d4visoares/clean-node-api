9739f05da8fff128b407f99856f323c0d6ab1bee
Davi-Soares-Silva <davisoares4456@gmail.com>
2023-07-20 20:50:49 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,14 @@
+import { LoadAccountByEmailRepository } from '@/data/protocols/db';
+import { Authentication, AuthenticationModel } from '@/domain/usecases';
+
+export class DbAuthentication implements Authentication {
+  constructor(
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+  ) {}
+
+  async auth(authentication: AuthenticationModel): Promise<string | null> {
+    await this.loadAccountByEmailRepository.load(authentication.email);
+
+    return null;
+  }
+}
