8377ddfb2b0165e04967930637f9650efee82814
Davi-Soares-Silva <davisoares4456@gmail.com>
2023-07-20 21:31:21 -0300

    feat: ensure DbAuthentication calls TokenGenerator with correct id

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,11 +1,12 @@
-import { HashComparer } from '@/data/protocols/cryptography';
+import { HashComparer, TokenGenerator } from '@/data/protocols/cryptography';
 import { LoadAccountByEmailRepository } from '@/data/protocols/db';
 import { Authentication, AuthenticationModel } from '@/domain/usecases';
 
 export class DbAuthentication implements Authentication {
   constructor(
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly tokenGenerator: TokenGenerator
   ) {}
 
   async auth(authentication: AuthenticationModel): Promise<string | null> {
@@ -15,7 +16,8 @@ export class DbAuthentication implements Authentication {
 
     if (!account) return null;
 
-    this.hashComparer.compare(authentication.password, account.password);
+    await this.hashComparer.compare(authentication.password, account.password);
+    await this.tokenGenerator.generate(account.id);
 
     return null;
   }
