9dbd8ed3940af77c5ff090ef58fa4a989ebe7222
Davi-Soares-Silva <davisoares4456@gmail.com>
2023-07-24 20:31:29 -0300

    test: ensure BcryptAdapter returns true when compare success

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -45,6 +45,14 @@ describe('Bcrypt Adapter', () => {
     expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash');
   });
 
+  test('Should return true when compare success', async () => {
+    const sut = makeSut();
+
+    const isValid = await sut.compare('any_value', 'any_hash');
+
+    expect(isValid).toBe(true);
+  });
+
   test('Should throw if bcrypt throws', async () => {
     const sut = makeSut();
 
