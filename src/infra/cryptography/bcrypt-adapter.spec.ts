75a891cc1eafa3d4e3d327e44eb2f2162c5a7fd1
Davi-Soares-Silva <davisoares4456@gmail.com>
2023-07-24 20:46:10 -0300

    test: ensure BcryptAdapter throw if compare throws

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -65,7 +65,19 @@ describe('Bcrypt Adapter', () => {
     expect(isValid).toBe(false);
   });
 
-  test('Should throw if bcrypt throws', async () => {
+  test('Should throw if hash throws', async () => {
+    const sut = makeSut();
+
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const promise = sut.hash('any_value');
+
+    expect(promise).rejects.toThrow();
+  });
+
+  test('Should throw if compare throws', async () => {
     const sut = makeSut();
 
     jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
