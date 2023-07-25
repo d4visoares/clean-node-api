e311c4a709d2ff89f4b6e0637f1ec28d5261aaac
Davi-Soares-Silva <davisoares4456@gmail.com>
2023-07-24 21:07:00 -0300

    feat: ensure JwtAdaptercalls sign with correct values

---
+++ src/infra/cryptography/jwt/jwt-adapter.ts
@@ -0,0 +1,17 @@
+import { Encrypter } from '@/data/protocols/cryptography';
+import jwt from 'jsonwebtoken';
+
+export class JwtAdapter implements Encrypter {
+  constructor(private readonly secret: string) {}
+
+  async encrypt(value: string): Promise<string> {
+    jwt.sign(
+      {
+        id: value,
+      },
+      this.secret
+    );
+
+    return new Promise((resolve) => resolve(''));
+  }
+}
