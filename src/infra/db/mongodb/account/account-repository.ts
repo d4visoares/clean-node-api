9a49da5945ff878956c09fe350c1fef2a631d92e
Davi-Soares-Silva <davisoares4456@gmail.com>
2023-07-24 21:31:24 -0300

    feat: ensure LoadAccountByEmailRepository returns an account on success

--- src/infra/db/mongodb/account/account-repository.ts
+++ src/infra/db/mongodb/account/account-repository.ts
@@ -1,4 +1,7 @@
-import { AddAccountRepository } from '@/data/protocols/db';
+import {
+  AddAccountRepository,
+  LoadAccountByEmailRepository,
+} from '@/data/protocols/db';
 import { AccountModel } from '@/domain/models';
 import { AddAccountModel } from '@/domain/usecases';
 
@@ -6,7 +9,23 @@ import { MongoHelper } from '../utils';
 
 const mongoHelper = MongoHelper.getInstance();
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository
+  implements AddAccountRepository, LoadAccountByEmailRepository
+{
+  async loadByEmail(email: string): Promise<AccountModel | null> {
+    const accountCollection = await mongoHelper.getCollection('accounts');
+
+    if (!accountCollection) throw new Error('COLLECTION_NOT_FOUND');
+
+    const loadedAccount = await accountCollection.findOne({ email });
+
+    if (!loadedAccount) return null;
+
+    const account = mongoHelper.map(loadedAccount);
+
+    return account;
+  }
+
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await mongoHelper.getCollection('accounts');
 
